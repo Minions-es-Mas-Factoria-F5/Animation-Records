3dfc70b2e811e76aa354340955731d191425c2df
irina-p-d <67954525+irina-p-d@users.noreply.github.com>
2021-11-05 19:43:43 +0100

    Update slideshow.js

--- scripts/modules/slideshow.js
+++ scripts/modules/slideshow.js
@@ -1,10 +1,11 @@
 let i=0;
-let imagesSlider = ["/assets/img/banner/marvel-banner-opcion1.jpg", "/assets/img/banner/marvel-banner-opcion2.jpg", "/assets/img/banner/marvel-banner-opcion3.jpg"];
-                                 
-export function changeImage() {
-    document.imgSlideshow.src = imagesSlider[i];
 
-    if (i < imagesSlider.length - 1) {
+export function changeImage() {
+  let images = ["/assets/img/banner/marvel-banner-opcion1.jpg", "/assets/img/banner/marvel-banner-opcion2.jpg", "/assets/img/banner/marvel-banner-opcion3.jpg"];
+                                 
+    document.imgSlideshow.src = images[i];
+
+    if (i < images.length - 1) {
         i++;
     } 
     else {
