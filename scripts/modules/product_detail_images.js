09d80627bc2b399cc6ebe29a34c46223a592db46
irina-p-d <67954525+irina-p-d@users.noreply.github.com>
2021-11-05 19:46:09 +0100

    Update product_detail_images.js

--- scripts/modules/product_detail_images.js
+++ scripts/modules/product_detail_images.js
@@ -2,20 +2,20 @@ let bigImage = document.getElementById("product-mainImage");
 let smallImage1 = document.getElementById("min-image-1");
 let smallImage2 = document.getElementById("min-image-2");
 let smallImage3 = document.getElementById("min-image-3");
-let images = ["/assets/img/product-details/img-original-1.jpg", "/assets/img/product-details/img-original-2.jpg", "/assets/img/product-details/img-original-3.jpg"];
+let imagesP = ["/assets/img/product-details/img-original-1.jpg", "/assets/img/product-details/img-original-2.jpg", "/assets/img/product-details/img-original-3.jpg"];
 
 export function changeImagesDetails (){
 
         smallImage1.addEventListener("click", () => 
-                bigImage.src = images[0]
+                bigImage.src = imagesP[0]
         
         );
         smallImage2.addEventListener("click", () => 
-                bigImage.src = images[1]
+                bigImage.src = imagesP[1]
         
         );
         smallImage3.addEventListener("click", () => 
-                bigImage.src = images[2]
+                bigImage.src = imagesP[2]
         
         );
 }
