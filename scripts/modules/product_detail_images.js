18415a9e186d262f0d9032e42154b25cecb9ee9c
irina-p-d <67954525+irina-p-d@users.noreply.github.com>
2021-11-06 02:04:27 +0100

    C

--- scripts/modules/product_detail_images.js
+++ scripts/modules/product_detail_images.js
@@ -2,7 +2,7 @@ let bigImage = document.getElementById("product-mainImage");
 let smallImage1 = document.getElementById("min-image-1");
 let smallImage2 = document.getElementById("min-image-2");
 let smallImage3 = document.getElementById("min-image-3");
-let images = [".assets/img/product-details/img-original-1.jpg", ".assets/img/product-details/img-original-2.jpg", ".assets/img/product-details/img-original-3.jpg"];
+let images = ["./assets/img/product-details/img-original-1.jpg", "./assets/img/product-details/img-original-2.jpg", "./assets/img/product-details/img-original-3.jpg"];
 
 export function changeImagesDetails (){
 
