684c948738cdb87de70052e7bbfdd6a448cacc99
irina-p-d <67954525+irina-p-d@users.noreply.github.com>
2021-11-06 01:28:07 +0100

    Update

--- scripts/modules/slideshow.js
+++ scripts/modules/slideshow.js
@@ -1,5 +1,5 @@
 let i=0;
-let images = ["./../../assets/img/banner/marvel-banner-opcion1.jpg", "assets/img/banner/marvel-banner-opcion2.jpg", ".assets/img/banner/marvel-banner-opcion3.jpg"];
+let images = [".assets/img/banner/marvel-banner-opcion1.jpg", ".assets/img/banner/marvel-banner-opcion2.jpg", ".assets/img/banner/marvel-banner-opcion3.jpg"];
 export function changeImage() {
   
                                  
